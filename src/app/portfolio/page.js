fd8c33b393f561c10cdde48c7385b21367cdfe8e
JASDA0000 <103983336+JASDA0000@users.noreply.github.com>
2025-05-04 17:10:56 +0700

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../../components/Navbar'
 import Image from 'next/image'
-import LinkButton from '../../../components/LinkButton'
+import LinkButton from '../../../components/Linkbutton'
 
 const page = () => {
   return (
