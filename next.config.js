06db4249f6c89eb261437dc3b257260b85fa33c4
JASDA0000 <103983336+JASDA0000@users.noreply.github.com>
2025-05-04 17:32:40 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-// next.config.js
+const path = require('path');
+
 module.exports = {
     reactStrictMode: true,
     eslint: {
-      ignoreDuringBuilds: true,
+        ignoreDuringBuilds: true,
     },
-  }
-  
+    webpack(config) {
+        config.resolve.alias['@components'] = path.resolve(__dirname, 'src/components');
+        return config;
+    },
+};
